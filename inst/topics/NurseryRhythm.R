7dc703b0c285a8f257021431a29d6e8caa0d0770
lily6966 <lli53@users.noreply.github.com>
2021-04-13 02:04:50 -0700

    Create a new bingo card with nursery rhythm topic

---
+++ inst/topics/NurseryRhythm.R
@@ -0,0 +1,29 @@
+## Aim : bingo card proposal for Nursery Rhythm
+## Author : Liying Li
+  c(
+    "Baby Shark",
+    "Five Little Monkey Jumping on the Bed",
+    "Daisy Daisy",
+    "Wheel on the Bus",
+    "London Bridge Falling Down",
+    "Skidamarink",
+    "I Caught a Fish Alive",
+    "Ten Sausages in a Pan",
+    "John Jacob",
+    "Mary HaD a Little Lamb",
+    "ABC Phonics Song",
+    "Rain Rain Go Away",
+    "Finger Family",
+    "Ants Go Marching",
+    "Old Macdonald",
+    "Itsy Bitsy Spider",
+    "I Love the Mountains",
+    "Three Little Pigs",
+    "Hickory Dickory Dock",
+    "Jack and Jill",
+    "Baa Baa Black Sheep",
+    "Muffin Man",
+    "Color Popsicles",
+    "My Dog Name is Bingo"
+  )
+    

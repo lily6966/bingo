a4bebccde291ba2ba45083078503ba1232d9af54
lily6966 <lli53@users.noreply.github.com>
2021-04-13 03:10:53 -0700

    New stat-test topic

---
+++ inst/topics/stat-test.R
@@ -0,0 +1,28 @@
+## Aim : bingo card proposal for Environmental Data Analysis
+## Author : Liying Li
+c(
+  "Fourier Transform",
+  "Maximum Likelihood Estimation",
+  "Structural Equation Models",
+  "Gaussian Process",
+  "Categorial Data",
+  "Cencored Data",
+  "Multidimentional Scaling",
+  "Kendall Tau",
+  "Peason's correlation",
+  "ANOVA",
+  "PERMANOVA",
+  "Principle Component Analysis",
+  "ANCOVA",
+  "Random Forest",
+  "OLS",
+  "Neural Networks",
+  "Canonical Correlation",
+  "Permutation Test",
+  "Kruskal-Wallis Test",
+  "Sample Size",
+  "Wilcoxon Rank-sum test",
+  "Sign-test",
+  "Studentized T-test",
+  "Contingency Tables"
+)
